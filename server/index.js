d62b66474f53ac1bd9a254ccac592e0de6478b86
Nasa Quba <nasaquba@gmail.com>
2019-01-06 21:25:09 -0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use('/', express.static(path.join(__dirname, '../client/public')))
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
